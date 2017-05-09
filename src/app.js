278f0a281902d3ed43cd9ac9be2434642e5a9cf4
donestre <donestre17@gmail.com>
2017-05-09 17:28:22 +0800

    test

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import React,{ Component } from 'react';
+import { render } from 'react-dom'
+
+require('./style/style.css')
+
+class MyApp extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count:0,
+            items:[
+                'no1',
+                'no2',
+                'no3'
+            ]
+        }
+        this.clickhandle = this.clickhandle.bind(this)
+    }
+    clickhandle(ev){
+        console.log(ev.target)
+        console.log('click!!')
+        let num = this.state.count;
+        let newArr = [...this.state.items,'no'+((++num)+3)]
+        this.setState({
+            count:num,
+            items:newArr
+        })
+    }
+    render(){
+        let data = this.state
+        return <h1 onClick={this.clickhandle}>{
+                data.items.map((e,i)=>{
+                    return (<p key={i}>{e}</p>)
+                })
+            }</h1>
+    }
+}
+
+render(
+    <MyApp mes="test"/>,
+    document.getElementById('root')
+)
